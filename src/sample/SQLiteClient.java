c7a43654baa7052b6cd911720915bdaa408ebd10
Alexhlo <Hlopotnow.alek@yandex.ru>
2019-05-17 13:10:08 +0300

    Продолжаю разбираться с кодом

--- src/sample/SQLiteClient.java
+++ src/sample/SQLiteClient.java
@@ -1,23 +1,18 @@
 package sample;
 
-import javafx.collections.ObservableList;
-import javafx.scene.control.TableView;
-import pojo.Product;
-import sample.controllers.ProductCalControl;
-
 import java.sql.*;
 
 public class SQLiteClient {
 
-    public static String HOST = "jdbc:sqlite:Calorifier.db";
     public static Connection connection;
-    public static Statement statement;
+    private static Statement statement;
     public static ResultSet resultSet;
 
     public static void connectDB() throws ClassNotFoundException, SQLException {
         connection = null;
         Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection(HOST);
+        String host = "jdbc:sqlite:Calorifier.db";
+        connection = DriverManager.getConnection(host);
         statement = connection.createStatement();
         System.out.println("Database connection has been done.");
     }
@@ -26,44 +21,39 @@ public class SQLiteClient {
         statement.close();
         resultSet.close();
         connection.close();
-        if(resultSet.isClosed()){
-            System.out.println("Database result set close.");
-        }
-        if (statement.isClosed()) {
-            System.out.println("Database statement close");
-        }if (connection.isClosed()) {
-            System.out.println("Database Connection close");
+        if(resultSet.isClosed() && statement.isClosed() && connection.isClosed()){
+            System.out.println("---= Database RS,STMNT,CON is closed! =---");
         }
     }
 
-    public static void addIntoDB() {
-
-    }
-
-    public static void deleteFromDB() {
-
-    }
-
-    public static void readDB(String request , TableView<Product> tableView, ObservableList<Product> observableList) {
-        try {
-           resultSet = connection.createStatement().executeQuery(request);
-            while (resultSet.next()) {
-                Product product = new Product();
-                product.name.set(SQLiteClient.resultSet.getString(Const.TABLE_NAME));
-                product.protein.set(SQLiteClient.resultSet.getDouble(Const.TABLE_PROTEIN));
-                product.fats.set(SQLiteClient.resultSet.getDouble(Const.TABLE_FATS));
-                product.carbs.set(SQLiteClient.resultSet.getDouble(Const.TABLE_CARBS));
-                product.calories.set(SQLiteClient.resultSet.getInt(Const.TABLE_CALORIES));
-                observableList.add(product);
-                tableView.setItems(observableList);
-            }
-            System.out.println("-----------------=Таблица выведена=-----------------");
-
-//            SQLiteClient.closeDB();
-        } catch (SQLException e) {
-            e.getStackTrace();
-        }
-    }
+//    public static void addIntoDB() {
+//
+//    }
+//
+//    public static void deleteFromDB() {
+//
+//    }
+//
+//    public static void readDB(String request , TableView<Product> tableView, ObservableList<Product> observableList) {
+//        try {
+//           resultSet = connection.createStatement().executeQuery(request);
+//            while (resultSet.next()) {
+//                Product product = new Product();
+//                product.name.set(SQLiteClient.resultSet.getString(Const.TABLE_NAME));
+//                product.protein.set(SQLiteClient.resultSet.getDouble(Const.TABLE_PROTEIN));
+//                product.fats.set(SQLiteClient.resultSet.getDouble(Const.TABLE_FATS));
+//                product.carbs.set(SQLiteClient.resultSet.getDouble(Const.TABLE_CARBS));
+//                product.calories.set(SQLiteClient.resultSet.getInt(Const.TABLE_CALORIES));
+//                observableList.add(product);
+//                tableView.setItems(observableList);
+//            }
+//            System.out.println("-----------------=Таблица выведена=-----------------");
+//
+////            SQLiteClient.closeDB();
+//        } catch (SQLException e) {
+//            e.getStackTrace();
+//        }
+//    }
 }
 
 
