42b53ffa050cbeb6b25f57f8cf7467ea8068e055
Alexhlo <Hlopotnow.alek@yandex.ru>
2019-06-07 15:59:19 +0300

    Небольшая рокировка в папках произошла

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,25 @@
+package main.java.sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage caloStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../views/calcWindow.fxml"));
+        caloStage.setTitle("Калоризатор");
+        Scene calorizator = new Scene(root);
+        caloStage.setScene(calorizator);
+        caloStage.setResizable(false);
+        caloStage.show();
+        caloStage.setOnCloseRequest(event -> { Platform.exit();System.exit(0);});
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
